6f30cc7ab0ec2b93628e4ceb1f0ced8311faee80
Bassem1234 <bassem.rbaia1@gmail.com>
2021-12-21 15:46:25 +0100

    Add API root

--- index.js
+++ index.js
@@ -6,7 +6,32 @@ const port = 3000
 app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
-  res.send('Hello World world!')
+  res.json({message:'Hello World world!'})
+})
+
+//get all users
+app.get('/users', (req,res) => {
+    res.json({message:'all users'});
+})
+
+//get one user by id
+app.get('/users/:id', (req,res) => {
+    res.json({message:'get one user by his id'});
+})
+
+//add one user
+app.post('/users', (req,res) => {
+    res.json({message:'user added successfully'});
+})
+
+//update a user by id
+app.put('/users/:id', (req, res) => {
+    res.json({message:'update user by his id'});
+})
+
+//delet a user
+app.delete('/users/:id', (req, res) => {
+    res.json({message:'user deleted successfully'});
 })
 
 app.listen(port, () => {
